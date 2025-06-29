98d92d8d097aed42acf5d5e3e3828d1706b7e2f3
Denis <mochalov.denis.v@gmail.com>
2025-06-29 13:48:32 +0500

    fix device key

--- ProjApp.Database/Entities/Configs/DeviceConfiguration.cs
+++ ProjApp.Database/Entities/Configs/DeviceConfiguration.cs
@@ -7,7 +7,7 @@ internal class DeviceConfiguration : IEntityTypeConfiguration<Device>
 {
     public void Configure(EntityTypeBuilder<Device> builder)
     {
-        builder.HasIndex(e => e.Serial)
+        builder.HasIndex(e => new { e.DeviceTypeNumber, e.Serial })
                .IsUnique();
     }
 }
