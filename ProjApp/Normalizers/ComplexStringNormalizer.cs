367d84e3ce0883726f8192a5f0a220579d577766
DenisMKAIR <mochalov@mkair.ru>
2025-07-01 16:24:10 +0500

    string norm fix

--- ProjApp/Normalizers/ComplexStringNormalizer.cs
+++ ProjApp/Normalizers/ComplexStringNormalizer.cs
@@ -5,8 +5,8 @@ public class ComplexStringNormalizer : IStringNormalizer
     private readonly IReadOnlyList<IStringNormalizer> _normalizers = [new SpaceNormalizer(), new QuoteNormalizer()];
     public string Normalize(string value)
     {
-        value = value.Trim().ToUpper();
         foreach (var normalizer in _normalizers) value = normalizer.Normalize(value);
+        value = value.Trim().ToUpper();
         return value;
     }
 }
