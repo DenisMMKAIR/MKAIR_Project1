68900c2d2cc3fe584e49b44dc7315db90d9e94fb
DenisMKAIR <mochalov@mkair.ru>
2025-07-29 13:35:22 +0500

    file exists check added

--- ProjApp/ProtocolForms/PDFFilePath.cs
+++ ProjApp/ProtocolForms/PDFFilePath.cs
@@ -1,14 +1,18 @@
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using ProjApp.Database.Entities;
 
 namespace ProjApp.ProtocolForms;
 
 public class PDFFilePathManager
 {
+    private readonly ILogger<PDFFilePathManager> _logger;
     private readonly string _protocolsDirPath;
 
-    public PDFFilePathManager(IConfiguration configuration)
+    public PDFFilePathManager(ILogger<PDFFilePathManager> logger, IConfiguration configuration)
     {
+        _logger = logger;
+
         _protocolsDirPath = configuration["ProtocolsExportDirPath"] ??
             throw new ArgumentNullException(nameof(configuration), "Директория протоколов не задана в настройках");
     }
@@ -27,6 +31,21 @@ public class PDFFilePathManager
         var mpi = form.VerifiedUntilDate.Year - form.VerificationDate.Year;
         var fileName = $"{form.VerificationDate:yyyy-MM-dd} № {form.DeviceSerial} (МПИ-{mpi}).pdf";
         fileName = Path.GetInvalidFileNameChars().Aggregate(fileName, (current, c) => current.Replace(c, '_'));
-        return Path.Combine(dirPath, fileName);
+
+        var filePath = Path.Combine(dirPath, fileName);
+
+        if (File.Exists(filePath))
+        {
+            _logger.LogWarning("Файл {fileName} уже существует", fileName);
+            var i = 2;
+
+            while (File.Exists(filePath) && i < 100)
+            {
+                fileName = $"{Path.GetFileNameWithoutExtension(fileName)} ({i++}).pdf";
+                filePath = Path.Combine(dirPath, fileName);
+            }
+        }
+
+        return filePath;
     }
 }
