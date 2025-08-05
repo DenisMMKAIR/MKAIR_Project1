ea74d1cec9ca4cdda9428f72cf989d122d169fd5
DenisMKAIR <mochalov@mkair.ru>
2025-08-05 11:19:03 +0500

    migrations

---
+++ ProjApp.Database.Maintenance/Migrations/20250805050835_ChangeCheckups1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjApp.Database.Maintenance.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCheckups1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "checkups_new",
+                table: "verification_methods",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "checkups_new",
+                table: "verification_methods");
+        }
+    }
+}
---
+++ ProjApp.Database.Maintenance/Migrations/20250805060629_ChangeCheckups2Final.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjApp.Database.Maintenance.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCheckups2Final : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "checkups",
+                table: "verification_methods");
+                
+            migrationBuilder.RenameColumn(
+                name: "checkups_new",
+                table: "verification_methods",
+                newName: "checkups");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // rename column checkups to checkup_new
+            migrationBuilder.RenameColumn(
+                name: "checkups",
+                table: "verification_methods",
+                newName: "checkup_new");
+        }
+    }
+}
